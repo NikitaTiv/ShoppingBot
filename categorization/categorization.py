ef761d000a8d54451c0bc764968ccf051df906c9
Vladislav Lukashin <agestop1@rambler.ru>
2022-12-18 14:36:02 +0300

    Перенес utils в categorization, поменял аннотации, испольховал метод items

--- categorization/categorization.py
+++ categorization/categorization.py
@@ -1,25 +1,20 @@
-import sys
-sys.path.append("../utils/")
-import json_func
+from utils import json_func
 
 
 def define_category(input_categories: dict, good: dict) -> str | None:
     result_category = None
     good_without_separator = good["name"].replace(".", " ").replace(",", " ").replace("/", " ")
     good_as_list = good_without_separator.lower().split()
-    tuple_categories = input_categories.items() 
-    for category in tuple_categories: 
-        category_set = set(category[1])
+    for category_name, category_words in input_categories.items(): 
+        category_set = set(category_words)
         if category_set.intersection(good_as_list):
-            result_category = category[0]
+            result_category = category_name
             break
     return result_category
 
 
-def add_categories_to_receipt(input_categories: dict, receipt: list) -> list:
-    for good in receipt:
-        if 'name' not in good: 
-            continue
+def add_categories_to_receipt(input_categories: dict, receipt: dict) -> dict:
+    for good in receipt["positions"]:
         good["category"] = define_category(input_categories, good)
     return receipt
 
@@ -27,5 +22,5 @@ def add_categories_to_receipt(input_categories: dict, receipt: list) -> list:
 if __name__ == "__main__":
     categories = json_func.read("categories.json")
     input_receipt = json_func.read("verified_receipt.json")
-    str_with_categories = add_categories_to_receipt(categories, input_receipt)
-    json_func.write("verified_receipt_with_categories.json", str_with_categories)
+    receipt_with_categories = add_categories_to_receipt(categories, input_receipt)
+    json_func.write("verified_receipt_with_categories.json", receipt_with_categories)
