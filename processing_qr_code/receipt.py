1121f72fd9f42c3ba47bda217501332e9011523a
Alexander Levkin <49125444+Comandosss@users.noreply.github.com>
2022-12-22 00:19:22 +0300

    Replaced quotes and the words: ticket and chek, removed loops, changed and made more readable function treat_products

--- processing_qr_code/receipt.py
+++ processing_qr_code/receipt.py
@@ -3,35 +3,32 @@ import json
 from typing import Any
 
 
-def treat_check(ticket_data: dict[str, Any]) -> dict[str, Any]:
-    '''Обрабатываем данные чека'''
-    data_check = {}
-    data_products = ticket['ticket']['document']['receipt']['items']
-    select_data_check = {
-        'operation': ticket['operation']['date'],
-        'seller': ticket['seller'].get('name'),
-        'id': ticket['id']}
-    for head_check in ticket_data:
-        if head_check in select_data_check:
-            data_check[f'{head_check}'] = select_data_check[f'{head_check}']
-        elif head_check == 'ticket':
-            data_check['positions'] = treat_products(data_products)
-    return data_check
+def treat_receipt(data: dict[str, Any]) -> dict[str, Any]:
+    """Обрабатываем данные чека"""
+    data_receipt = {}
+    data_products = data['ticket']['document']['receipt']['items']
+    select_data_receipt = {
+        'operation': data['operation']['date'],
+        'seller': data['seller'].get('name'),
+        'id': data['id']
+    }
+    for head_receipt in data:
+        if head_receipt in select_data_receipt:
+            data_receipt[f'{head_receipt}'] = select_data_receipt[f'{head_receipt}']
+        elif head_receipt == 'ticket':
+            data_receipt['positions'] = treat_products(data_products)
+    return data_receipt
 
 
 def treat_products(data_product: list[dict[str, Any]]) -> list[dict[str, Any]]:
-    '''Обрабатываем позиции продукта в чеке'''
+    """Обрабатываем позиции продукта в чеке"""
     list_products = []
-    selected_head_product = ['name', 'price', 'quantity', 'sum']
-    for _, positions in enumerate(data_product):
+    for positions in data_product:
         product = {}
-        for head_product in positions:
-            if head_product in selected_head_product:
-                product[head_product] = positions[head_product]
-                if head_product == 'price':
-                    product[head_product] = (positions[head_product]) / 100
-                elif head_product == 'sum':
-                    product[head_product] = (positions[head_product]) / 100
+        product['name'] = positions['name']
+        product['price'] = positions['price'] / 100
+        product['quantity'] = positions['quantity']
+        product['sum'] = positions['sum'] / 100
         list_products.append(product)
     return list_products
 
@@ -40,8 +37,8 @@ if __name__ == '__main__':
     client = NalogRuPython()
     qr_code = \
         't=20221210T2003&s=1529.93&fn=9960440502976281&i=756&fp=2190851534&n=1'
-    ticket = client.get_ticket(qr_code)
+    receipt = client.get_ticket(qr_code)
     client.refresh_token_function()
-    processed_check = treat_check(ticket)
-    with open('verified_check.json', 'w', encoding='utf-8') as w_file:
-        w_file.write(json.dumps(processed_check, indent=4, ensure_ascii=False))
+    processed_receipt = treat_receipt(receipt)
+    with open('verified_receipt.json', 'w', encoding='utf-8') as w_file:
+        w_file.write(json.dumps(processed_receipt, indent=4, ensure_ascii=False))
