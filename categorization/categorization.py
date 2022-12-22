12450f2afbd7aa1713caac906e8f9c1ba195d66d
Vladislav Lukashin <agestop1@rambler.ru>
2022-12-22 18:01:27 +0300

    Поменял input_categories на categories_corpus

--- categorization/categorization.py
+++ categorization/categorization.py
@@ -2,11 +2,11 @@ from utils import json_func
 from typing import Any
 
 
-def define_category(input_categories: dict, good: dict) -> str | None:
+def define_category(categories_corpus: dict, good: dict) -> str | None:
     result_category = None
     good_without_separator = good["name"].replace(".", " ").replace(",", " ").replace("/", " ")
     good_as_list = good_without_separator.lower().split()
-    for category_name, category_words in input_categories.items(): 
+    for category_name, category_words in categories_corpus.items(): 
         category_set = set(category_words)
         if category_set.intersection(good_as_list):
             result_category = category_name
@@ -14,9 +14,9 @@ def define_category(input_categories: dict, good: dict) -> str | None:
     return result_category
 
 
-def add_categories_to_receipt(input_categories: dict, receipt: dict) -> dict:
+def add_categories_to_receipt(categories_corpus: dict, receipt: dict) -> dict:
     for good in receipt["positions"]:
-        good["category"] = define_category(input_categories, good)
+        good["category"] = define_category(categories_corpus, good)
     return receipt
 
 
