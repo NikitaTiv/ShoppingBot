93c3ba20027e503d6ed2870e3666f0878b0463ee
Vladislav Lukashin <agestop1@rambler.ru>
2022-12-23 16:53:04 +0300

    Убрал дуплекс

--- database/models.py
+++ database/models.py
@@ -1,17 +1,29 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from db import Base, engine
+from sqlalchemy.orm import relationship
 
 
 class User(Base):
-    __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    name_photo = Column(String)
-    date_upload = Column(DateTime)
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True, autoincrement="auto")
+    name = Column(String(255), nullable=False)
 
 
     def __repr__(self):
-        return f'Участник под номером {self.id} - {self.name}'
+        return f'Пользователь id={self.id} - {self.name}'
+
+
+class Receipt(Base):
+    __tablename__ = 'receipt'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), unique=True, nullable=False)
+    date_upload = Column(DateTime)
+    user_id = Column(Integer, ForeignKey("user.id"))
+    user = relationship("User")
+
+
+    def __repr__(self):
+        return f'Чек id={self.id} - {self.name} был загружен {self.date_upload} пользователем {self.user_id}'
 
 
 if __name__ == "__main__":
