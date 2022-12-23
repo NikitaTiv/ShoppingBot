f77f2d570d656714c564d7f13ce26e0a24526432
Vladislav Lukashin <agestop1@rambler.ru>
2022-12-23 19:59:20 +0300

    Убрал из модели объект класса User

--- database/models.py
+++ database/models.py
@@ -3,12 +3,10 @@ from db import Base, engine
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 
-
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True, autoincrement="auto")
     name = Column(String(255), nullable=False)
-
 
     def __repr__(self):
         return f'Пользователь id={self.id} - {self.name}'
@@ -19,9 +17,7 @@ class Receipt(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(255), unique=True, nullable=False)
     date_upload = Column(DateTime(timezone=True), server_default=func.now())
-    user_id = Column(Integer, ForeignKey("user.id"))
-    user = relationship("User")
-
+    user_id = Column(Integer, ForeignKey(User.id), index=True)
 
     def __repr__(self):
         return f'Чек id={self.id} - {self.name} был загружен {self.date_upload} пользователем {self.user_id}'
