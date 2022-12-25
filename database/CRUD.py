62b1d1c169fd33ec02dde15eda05d32cf8155bfa
Vladislav Lukashin <agestop1@rambler.ru>
2022-12-25 17:50:22 +0300

    input_id -> user_id or receipt_id

--- database/CRUD.py
+++ database/CRUD.py
@@ -8,48 +8,48 @@ def add_user(user_name: str) -> None:
     db_session.commit()
 
 
-def get_user_info(input_id: int) -> str|None:
-    if db_session.query(User.query.filter(User.id == input_id).exists()).scalar():
-        user = User.query.get(input_id)
-        return f'Имя пользоватя id №{input_id} - {user.name}'
+def get_user_info(user_id: int) -> str|None:
+    if db_session.query(User.query.filter(User.id == user_id).exists()).scalar():
+        user = User.query.get(user_id)
+        return f'Имя пользоватя id №{user_id} - {user.name}'
     else:
         return None
 
 
-def update_user_info(input_id: int, input_name: str) -> None:
-    user = User.query.get(input_id)
-    user.name = input_name
+def update_user_info(user_id: int, user_name: str) -> None:
+    user = User.query.get(user_id)
+    user.name = user_name
     db_session.commit()
 
 
-def delete_user(input_id: int) -> None:
-    user = User.query.get(input_id)
+def delete_user(user_id: int) -> None:
+    user = User.query.get(user_id)
     db_session.delete(user)
     db_session.commit()
 
 
-def add_receipt(receipt_name: str, input_user_id: int) -> None:
-    receipt = Receipt(name=receipt_name, user_id=input_user_id)
+def add_receipt(receipt_name: str, user_id: int) -> None:
+    receipt = Receipt(name=receipt_name, user_id=user_id)
     db_session.add(receipt)
     db_session.commit()
 
 
-def get_receipt_info(input_id: int) -> str|None:
-    if db_session.query(Receipt.query.filter(Receipt.id == input_id).exists()).scalar():
-        receipt = Receipt.query.get(input_id)
+def get_receipt_info(receipt_id: int) -> str|None:
+    if db_session.query(Receipt.query.filter(Receipt.id == receipt_id).exists()).scalar():
+        receipt = Receipt.query.get(receipt_id)
         user = User.query.get(receipt.user_id)
-        return(f'Чек id №{input_id} с названием "{receipt.name}" загружен пользователем id №{receipt.user_id} по имени {user.name} {receipt.date_upload}')
+        return(f'Чек id №{receipt_id} с названием "{receipt.name}" загружен пользователем id №{receipt.user_id} по имени {user.name} {receipt.date_upload}')
     else:
         return None
 
 
-def update_receipt_info(input_id: int, input_name: str) -> None:
-    receipt = Receipt.query.get(input_id)
-    receipt.name = input_name
+def update_receipt_info(receipt_id: int, receipt_name: str) -> None:
+    receipt = Receipt.query.get(receipt_id)
+    receipt.name = receipt_name
     db_session.commit()
 
 
-def delete_receipt(input_id: int) -> None:
-    receipt = Receipt.query.get(input_id)
+def delete_receipt(receipt_id: int) -> None:
+    receipt = Receipt.query.get(receipt_id)
     db_session.delete(receipt)
     db_session.commit()
