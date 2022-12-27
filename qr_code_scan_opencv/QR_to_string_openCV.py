a9a7ddcd6e3e688619bfab9429f40e966fe8e530
Alexander Levkin <49125444+Comandosss@users.noreply.github.com>
2022-12-28 00:49:12 +0300

    Добавил проверку изображения на QR-код и формат кассового чека

--- qr_code_scan_opencv/QR_to_string_openCV.py
+++ qr_code_scan_opencv/QR_to_string_openCV.py
@@ -6,10 +6,16 @@ def read_qr_code(filepath: str) -> str:
         img = cv2.imread(filepath)
         detect = cv2.QRCodeDetector()
         value, points, straight_qrcode = detect.detectAndDecode(img)
-        return value
-    except cv2.error as error:
-        return error
+        if points is None:
+            return 'QR-код не обнаружен'
+        else:
+            codes_receipt = ['fn=', 'i=', 'fp=']
+            if all(tech_codes in value for tech_codes in codes_receipt):
+                return value
+            return 'QR-код не с кассового чека'
+    except cv2.error:
+        return 'Неверное имя/путь файла'
 
 
-if __name__ == "__main__":
-    print(read_qr_code("IMG_20221211_162336.jpg"))
+if __name__ == '__main__':
+    print(read_qr_code('photo_2022-12-23_15-15-29.jpg'))
