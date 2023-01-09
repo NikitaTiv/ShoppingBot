a5c3920ede169f6b846a2e47025c85fa55fa785b
Alexander Levkin <49125444+Comandosss@users.noreply.github.com>
2023-01-09 17:03:13 +0300

    feature/check_image_receipt (#12)
    * Добавил проверку изображения на QR-код и формат кассового чека
    
    * Исправил возвращаемое значение на None и тип на Any
    
    * Исправил аннотацию и результат работы функции

--- qr_code_scan_opencv/QR_to_string_openCV.py
+++ qr_code_scan_opencv/QR_to_string_openCV.py
@@ -1,15 +1,20 @@
 import cv2
 
 
-def read_qr_code(filepath: str) -> str:
+def read_qr_code(filepath: str) -> str | None:
     try:
         img = cv2.imread(filepath)
         detect = cv2.QRCodeDetector()
         value, points, straight_qrcode = detect.detectAndDecode(img)
-        return value
-    except cv2.error as error:
-        return error
+        if points is None:
+            return None
+        codes_receipt = ['fn=', 'i=', 'fp=']
+        if all(tech_codes in value for tech_codes in codes_receipt):
+            return value
+        return None
+    except cv2.error:
+        return None
 
 
-if __name__ == "__main__":
-    print(read_qr_code("IMG_20221211_162336.jpg"))
+if __name__ == '__main__':
+    print(read_qr_code('photo_2022-12-23_15-15-29.jpg'))
