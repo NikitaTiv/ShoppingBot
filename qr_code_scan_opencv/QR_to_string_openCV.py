c5c19f2c74e560a77670f515fd1d8403ff42fecc
Alexander Levkin <49125444+Comandosss@users.noreply.github.com>
2023-01-09 15:53:28 +0300

    Исправил аннотацию и результат работы функции

--- qr_code_scan_opencv/QR_to_string_openCV.py
+++ qr_code_scan_opencv/QR_to_string_openCV.py
@@ -1,21 +1,19 @@
 import cv2
-from typing import Any
 
 
-def read_qr_code(filepath: str) -> Any:
+def read_qr_code(filepath: str) -> str | None:
     try:
         img = cv2.imread(filepath)
         detect = cv2.QRCodeDetector()
         value, points, straight_qrcode = detect.detectAndDecode(img)
         if points is None:
             return None
-        else:
-            codes_receipt = ['fn=', 'i=', 'fp=']
-            if all(tech_codes in value for tech_codes in codes_receipt):
-                return value
-            return 'QR-код не с кассового чека'
+        codes_receipt = ['fn=', 'i=', 'fp=']
+        if all(tech_codes in value for tech_codes in codes_receipt):
+            return value
+        return None
     except cv2.error:
-        return 'Неверное имя/путь файла'
+        return None
 
 
 if __name__ == '__main__':
